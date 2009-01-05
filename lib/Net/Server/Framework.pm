16a9f591a370ceea8c5a4e9858875d63d43b6e0b
Lenz Gschwendtner <lenz@springtimesoft.com>
2009-01-05 14:32:04 +1300

    empty skeleton

---
+++ lib/Net/Server/Framework.pm
@@ -0,0 +1,107 @@
+package Net::Server::Framework;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Net::Server::Framework - The great new Net::Server::Framework!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::Server::Framework;
+
+    my $foo = Net::Server::Framework->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Lenz Gschwendtner, C<< <lenz at springtimesoft.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-server-framework at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Server-Framework>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Server::Framework
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-Server-Framework>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-Server-Framework>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Net-Server-Framework>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Net-Server-Framework>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Lenz Gschwendtner, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Net::Server::Framework
